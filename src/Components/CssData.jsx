12861348b6438a41e45dd73688654d1d5b813ec3
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-08-03 03:04:18 +0800

    add CSS Data

---
+++ src/Components/CssData.jsx
@@ -0,0 +1,16 @@
+export const defaultCssData = [
+    `h1 {
+        font-size: 30px;
+        color: red;
+    }`,
+    `#theId {
+        font-size: 25px;
+        color: green;
+        background-color: light-blue;
+    }`,
+    `.theClass{
+        font-size: 20px;
+        color: white;
+        background-color: black;
+    }`
+]
