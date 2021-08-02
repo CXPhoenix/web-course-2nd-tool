0e42518faff3114d38ebd064cdf40c25d9cfccb0
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-08-03 04:24:20 +0800

    fixed style

--- src/Components/EditorTags.jsx
+++ src/Components/EditorTags.jsx
@@ -4,6 +4,12 @@ import React from 'react'
 
 export default function EditorTags({ tagContent, tagId, clickEventHandler, isActive }) {
     return (
-        <button className={`tag py-2 px-1 mx-2 text-gray-50 focus:outline-none border-green-400 ${isActive ? "border-t-4" : ""}`} onClick={clickEventHandler} id={tagId}>{ tagContent }</button>
+        <button 
+        className={`tag py-2 px-1 mx-2 text-gray-50 focus:outline-none border-green-400 border-t-4 ${isActive ? "border-opacity-100" : "border-opacity-0"}`} 
+        onClick={clickEventHandler} 
+        id={tagId}
+        >
+            { tagContent }
+        </button>
     )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header({ courseTitle }) {
     return (
-        <header className={`w-full p-3 bg-blue-700 text-center`}>
-            <span className={`text-[4vw] text-gray-100`}>{ courseTitle }</span>
+        <header className={`w-full p-2 bg-blue-700 text-center`}>
+            <span className={`text-[3vw] text-gray-100`}>{ courseTitle }</span>
         </header>
     )
 }
