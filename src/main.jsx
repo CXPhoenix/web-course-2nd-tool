399581c622a572bb10a6e5c9a547f7149278ac8e
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-08-03 14:52:07 +0800

    fixed split js

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,22 @@ ReactDOM.render(
 
 let splitDirection = document.body.clientWidth >= 640 ? 'horizontal' : 'vertical'
 let cursor = splitDirection === 'vertical' ? 'col-resize' : 'row-resize'
-
+let minSize = splitDirection === 'vertical' ? 100 : 200
 Split(['#Editor', '#Preview'], {
-  minSize: 200,
+  minSize: minSize,
   gutterSize: 6,
   sizes: [35, 65],
   direction: splitDirection,
   cursor: cursor
 })
+
+
+// if (document.body.clientWidth >= 640) {
+//   Split(['#Editor', '#Preview'], {
+//     minSize: 200,
+//     gutterSize: 6,
+//     sizes: [35, 65],
+//     // direction: splitDirection,
+//     // cursor: cursor
+//   })
+// }
